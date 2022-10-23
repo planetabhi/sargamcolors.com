d4c95163ee5d78c1fa5b8dedde1b9ee2054e1f33
Abhimanyu Rana <abhimanyu.rana@postman.com>
2022-10-23 06:15:21 +0530

    theme fixes

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ const themeBtn = document.querySelector('.theme');
 
 function getCurrentTheme(){
   let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  localStorage.getItem('canabrey.theme') ? theme = localStorage.getItem('canabrey.theme') : null;
+  localStorage.getItem('sargam.theme') ? theme = localStorage.getItem('sargam.theme') : null;
   return theme;
 }
 
 function loadTheme(theme){
   const root = document.querySelector(':root');
   if(theme === "light"){
-    themeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" class="bkg2--stroke" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
+    themeBtn.innerHTML = `<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.41 13.28C7.332 10.205 6.716 5.693 8.357 2c-1.23.41-2.256 1.23-3.281 2.256a10.399 10.399 0 0 0 0 14.768c4.102 4.102 10.46 3.897 14.562-.205 1.026-1.026 1.846-2.051 2.256-3.282-3.896 1.436-8.409.82-11.486-2.256Z" stroke="var(--silver6)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>`;
   } else {
-    themeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" class="bkg2--stroke" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
+    themeBtn.innerHTML = `<svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 4V2M12 22v-2M4 12H2M22 12h-2M6.34 17.66l-1.41 1.41M19.07 4.93l-1.41 1.41M17.66 17.66l1.41 1.41M4.93 4.93l1.41 1.41" stroke="var(--silver4)" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round"/><path d="M12 16a4 4 0 1 0 0-8 4 4 0 0 0 0 8Z" stroke="var(--silver4)" stroke-width="1.5" stroke-miterlimit="10"/></svg>`;
   }
   root.setAttribute('color-scheme', `${theme}`);
 }
@@ -32,7 +32,7 @@ themeBtn.addEventListener('click', () => {
   }
   audio.currentTime = 0;
   audio.play();
-  localStorage.setItem('canabrey.theme', `${theme}`);
+  localStorage.setItem('sargam.theme', `${theme}`);
   loadTheme(theme);
 })
 
